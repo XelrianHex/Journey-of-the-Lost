e7b43053aba91ce7e3091a0b8e60c876713a9905
Xel <zachrrodgers@gmail.com>
2024-12-30 23:39:06 +0000

    added tailwind for Header component

--- jotl/components/header.tsx
+++ jotl/components/header.tsx
@@ -3,18 +3,18 @@ import Image from "next/image";
 
 const Header = () => {
     return (
-        <header id="header">
-            <h1 className="headerTitle">Journey of the Lost</h1>
-            <div id="stats">
-                <span className="stat">
+        <header id="header" className="flex flex-col items-center bg-dark text-white p-2.5 shadow-lg">
+            <h1 className="headerTitle text-2xl font-bold mb-2">Journey of the Lost</h1>
+            <div id="stats" className="flex gap-10">
+                <span className="stat flex items-center gap-10">
                     <Image src="experience-icon.svg" alt="exerpience-icon" width={34} height={34}/>
                     <strong>0</strong>
                 </span>
-                <span className="stat">
+                <span className="stat flex items-center gap-10">
                     <Image src="health-icon.svg" alt="health-icon" width={34} height={34}/>
                     <strong>100</strong>
                 </span>
-                <span className="stat">
+                <span className="stat flex items-center gap-10">
                     <Image src="gold-pouch-icon.svg" alt="gold-pouch-icon" width={34} height={34}/>
                     <strong>0</strong>
                 </span>
--- jotl/tailwind.config.ts
+++ jotl/tailwind.config.ts
@@ -9,9 +9,25 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-
+        dark: "var(--color-dark)",
+        grey: "var(--color-grey)",
+        darkGrey: "var(--color-dark-grey)",
+        offWhite: "var(--color-off-white)",
+        white: "var(--color-white)",
+        blue: "var(--color-blue)",
+        midnight: "var(--color-midnight)",
+        flatBlue: "var(--color-flat-blue)",
+        purple: "var(--color-purple)",
+        red: "var(--color-red)",
+        yellow: "var(--color-yellow)",
+        lightBlue: "var(--color-light-blue)",
+        black10: "var(--color-black-10)",
+        black20: "var(--color-black-20)",
+        black30: "var(--color-black-30)",
+        black50: "var(--color-black-50)",
+        black70: "var(--color-black-70)",
+        white10: "var(--color-white-10)",
+        white20: "var(--color-white-20)",
       },
     },
   },
