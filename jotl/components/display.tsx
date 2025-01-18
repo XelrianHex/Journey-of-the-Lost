365fbe4e4a1ab4cbede92de7adc2dfb8a193ea0a
Xelrian <zachrrodgers@gmail.com>
2025-01-17 23:48:35 -0500

    fixed autoprefixer, removed buttonnames, modified margins for display

--- jotl/components/display.tsx
+++ jotl/components/display.tsx
@@ -24,7 +24,7 @@ const Display: React.FC<DisplayProps> = ({displayState, combatState, buttonState
     return (
         <>
           <Combat combatState={combatState} buttonState={buttonState}/>
-          <div className="display-area m-24 block bg-black-10 border-2 border-black-20 p-5 rounded-lg shadow-md text-xl text-offWhite text-center flex-1 overflow-y-auto">
+          <div className="display-area m-0 block bg-black-10 border-2 border-black-20 p-5 rounded-lg shadow-md text-xl text-offWhite text-center flex-1 overflow-y-auto">
             <h2 id="chapter" className="text-2xl">{displayState.chapter}</h2>
             <h3 id="chapter-title" className="mt-4 text-4xl">{displayState.chapterTitle}</h3>
             <p id="text" className="mt-8">{displayState.text}</p>
